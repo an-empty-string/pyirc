ff8c4d96ae225670cab89b84fe675460d64f7a30
fwilson <fwilson@fwilson.me>
2014-07-09 10:44:56 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pyIRCframework',
-      version='0.0.1',
+      version='0.0.2',
       description='a Python IRC framework, with decorators and events!',
       author='Fox Wilson',
       author_email='fwilson@fwilson.me',
