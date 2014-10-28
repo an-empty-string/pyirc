06e6a9513bf497b4e4e05dd4aa9fe2269a51ddb7
fwilson <fwilson@fwilson.me>
2014-10-28 16:42:39 -0400

    add implicit whois support -- pyirc.user.User.from_nickname

--- pyirc/user.py
+++ pyirc/user.py
@@ -1,11 +1,34 @@
+import random
 class User:
-    def __init__(self, hostmask):
-        try:
-            self.nick, hostmask = hostmask.split("!")
-            self.user, self.host = hostmask.split("@")
-        except:
-            # if something has no user or host, it is likely the ircd
-            self.nick = self.user = self.host = "ircd"
+    def __init__(self, hostmask=None):
+        if hostmask:
+            try:
+                self.nick, hostmask = hostmask.split("!")
+                self.user, self.host = hostmask.split("@")
+            except:
+                # if something has no user or host, it is likely the ircd
+                # TODO this is hacky
+                self.nick = self.user = self.host = "ircd"
+
+    def from_nickname(self, conn, nickname, notify_callback=None):
+        """
+        Create a new user object (with user and host) based on a nickname.
+        This is done by calling whois from conn. A user might pass a function
+        to notify_callback, since user and host will not be available right
+        away.
+        """
+        user = User()
+        user.nick = nickname
+        callback_id = random.randint(1, 100000)
+        def whois_callback(conn, event):
+            if event.nick == user.nick:
+                user.user = event.user
+                user.host = event.host
+                if notify_callback:
+                    notify_callback(user)
+                # TODO implement a better way of self-unregistering
+                conn.unregister_callbacks("whois_%d" % callback_id)
+        conn.register_callback("whois-result", whois_callback, tag="whois_%d" % callback_id)
 
     def __repr__(self):
         return "%s!%s@%s" % (self.nick, self.user, self.host)
