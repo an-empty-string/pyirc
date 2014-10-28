0966997529809a4b3f61bee06f40d359d6e43181
fwilson <fwilson@fwilson.me>
2014-10-28 16:59:08 -0400

    package documentation

---
+++ pyirc/__init__.py
@@ -0,0 +1,10 @@
+"""
+PyIRC is an evented IRC client framework. It offers a number of features:
+
+    - built-in channel tracking (ops, voiced, users)!
+    - automatic channel joining!
+    - event handler attachment with decorators!
+    - documentation on events!
+    - server password support!
+    - (not yet) SASL authentication!
+"""
