fae897e760df0df7690c5f2cba7dc3fbe7969ea8
fwilson <fwilson@fwilson.me>
2014-10-28 16:50:55 -0400

    clearer variable names

--- pyirc/net.py
+++ pyirc/net.py
@@ -8,15 +8,15 @@ def do_connect(host, port):
     return s
 
 def do_incoming_listen(socket, callback):
-    def loop(s, c):
+    def loop(socket, callback):
         while True:
             cbuf = ""
             while len(cbuf) == 0 or cbuf[-1] != '\n':
                 try:
-                    cbuf += str(s.recv(1024), "utf-8")
+                    cbuf += str(socket.recv(1024), "utf-8")
                 except:
                     print("Bad: %s" % cbuf)
-            c(cbuf)
+            callback(cbuf)
 
     threading.Thread(target=loop, args=(socket, callback)).start()
 
