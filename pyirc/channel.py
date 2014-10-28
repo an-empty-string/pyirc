ed1e75b92a72317dcffa107e5518928cb45060a4
fwilson <fwilson@fwilson.me>
2014-10-28 16:50:16 -0400

    add explicit update names function

--- pyirc/channel.py
+++ pyirc/channel.py
@@ -4,6 +4,8 @@ class Channel:
         self.users = {}
         self.ops = []
         self.voices = []
+        self.conn = conn
+        self._callbacks = []
         conn.register_callback("names", self.names_hook, "nameshook_channel_%s" % name)
 
     def names_hook(self, conn, event):
@@ -11,3 +13,15 @@ class Channel:
         # TODO use ISUPPORT for ops/voice prefix
         self.ops = [i[0] for i in filter(lambda a: a[1] == "@", self.users.items())]
         self.voices = [i[0] for i in filter(lambda a: a[1] == "+", self.users.items())]
+        for i in self._callbacks:
+            i(self.users)
+        self._callbacks = []
+
+    def update_names(self, notify_callback=None):
+        """
+        Update the names cache for this channel. Opionally takes a
+        notify_callback for notification of update completion. The callback
+        will be passed the updated nicks dict.
+        """
+        self._callbacks.append(notify_callback)
+        self.conn.names(self.name)
