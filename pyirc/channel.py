e719b600bb1a411c273e9810a2f20b5a6f78bedb
fwilson <fwilson@fwilson.me>
2014-10-30 07:15:53 -0400

    add doc

--- pyirc/channel.py
+++ pyirc/channel.py
@@ -9,6 +9,10 @@ class Channel:
         conn.register_callback("names", self.names_hook, "nameshook_channel_%s" % name)
 
     def names_hook(self, conn, event):
+        """
+        Respond to incoming NAMES responses. This updates the channel's user
+        dict.
+        """
         self.users = event.nicks
         # TODO use ISUPPORT for ops/voice prefix
         self.ops = [i[0] for i in filter(lambda a: a[1] == "@", self.users.items())]
